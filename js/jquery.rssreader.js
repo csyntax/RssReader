7b519a3958015ed1e9d7e071ad573d5a5c7c979b
Ivan Cvetomirov Ivanov <slayer_word_11@outlook.com>
2015-06-30 09:03:57 +0300

    update callback and add functions for date and sort

--- js/jquery.rssreader.js
+++ js/jquery.rssreader.js
@@ -51,7 +51,7 @@
 					_process(e, data.responseData, options);
 
 					if ($.isFunction(fn)) {
-						fn.call(this,$(e));
+						fn.call(this, $(e));
 					}
 				} else {
 					if (options.showerror)
@@ -60,13 +60,14 @@
 						} else {
 							var msg = data.responseDetails;
 						};
+
 						$(e).html('<div class="rssError"><p>'+ msg +'</p></div>');
 				};
 			});
 		});
 	};
 
-	var _process = function(e, data, options) {
+function _process(e, data, options) {
 		var feeds = data.feed;
 		if (!feeds) {
 			return false;
@@ -80,119 +81,71 @@
 			var xml = _getXMLDocument(data.xmlString);
 			var xmlEntries = xml.getElementsByTagName('item');
 		}
-		// Todo
-		// Add header if required
-		if (options.header)
+
+		if (options.header) {
 			html +=	'<div class="rssHeader">' +
 				'<a href="'+feeds.link+'" title="'+ feeds.description +'">'+ feeds.title +'</a>' +
 				'</div>';
+	 }
 
-		// Add body
-		html += '<div class="rssBody">' +
-			'<ul>';
+		html += '<div class="rssBody">' + '<ul>';
 
-
-		// Add feeds
 		for (var i=options.offset; i<feeds.entries.length; i++) {
-
 			rowIndex = i - options.offset;
 			rowArray[rowIndex] = [];
 
-			// Get individual feed
 			var entry = feeds.entries[i];
 			var pubDate;
-			//var sort = '';
 			var feedLink = entry.link;
 
-			// Apply sort column
-			/*switch (options.sort) {
-				case 'title':
-					sort = entry.title;
-					break;
-				case 'date':
-					sort = entry.publishedDate;
-					break;
-			}
-			rowArray[rowIndex]['sort'] = sort;*/
-
-			// Format published date
 			if (entry.publishedDate) {
-
 				var entryDate = new Date(entry.publishedDate);
 				var pubDate = entryDate.toLocaleDateString() + ' ' + entryDate.toLocaleTimeString();
-
-
 			}
 
-			// Add feed row
-			if (options.linkredirect) feedLink = encodeURIComponent(feedLink);
-			rowArray[rowIndex]['html'] = '<'+ options.titletag +'><a href="'+ options.linkredirect + feedLink +'" title="View this feed at '+ feeds.title +'">'+ entry.title +'</a></'+ options.titletag +'>'
+			if (options.linkredirect) {
+				feedLink = encodeURIComponent(feedLink);
+			}
 
-			if (options.date && pubDate) rowArray[rowIndex]['html'] += '<div>'+ pubDate +'</div>'
+			rowArray[rowIndex]["html"] =
+				'<'+ options.titletag +'><a href="'+ options.linkredirect + feedLink +'" title="View this feed at '+
+				 feeds.title +'">'+ entry.title +'</a></'+ options.titletag +'>'
+
+			if (options.date && pubDate){
+				rowArray[rowIndex]["html"] += "<div>"+ pubDate + "</div>"
+			}
+
 			if (options.content) {
-
-				// Use feed snippet if available and optioned
-				if (options.snippet && entry.contentSnippet != '') {
+				if (options.snippet && entry.contentSnippet != "") {
 					var content = entry.contentSnippet;
 				} else {
 					var content = entry.content;
 				}
 
-				/*if (options.linkcontent) {
-					content = '<a href="'+ options.linkredirect + feedLink +'" title="View this feed at '+ feeds.title +'">'+ content +'</a>'
-				}*/
-
 				rowArray[rowIndex]['html'] += '<p>'+ content +'</p>'
 			}
 
-			// Add any media
 			if (options.media && xmlEntries.length > 0) {
-				var xmlMedia = xmlEntries[i].getElementsByTagName('enclosure');
-				if (xmlMedia.length > 0) {
+				var xmlMedia = xmlEntries[i].getElementsByTagName("enclosure");
 
+				if (xmlMedia.length > 0) {
 					rowArray[rowIndex]['html'] += '<div class="rssMedia"><div>Media files</div><ul>'
 
 					for (var m=0; m<xmlMedia.length; m++) {
 						var xmlUrl = xmlMedia[m].getAttribute("url");
 						var xmlType = xmlMedia[m].getAttribute("type");
 						var xmlSize = xmlMedia[m].getAttribute("length");
+
 						rowArray[rowIndex]['html'] += '<li><a href="'+ xmlUrl +'" title="Download this media">'+ xmlUrl.split('/').pop() +'</a> ('+ xmlType +', '+ _formatFilesize(xmlSize) +')</li>';
 					}
 					rowArray[rowIndex]['html'] += '</ul></div>'
 				}
 			}
-
 		}
 
-		// Sort if required
-	/*	if (options.sort) {
-			rowArray.sort(function(a,b) {
-
-				// Apply sort direction
-				if (options.sortasc) {
-					var c = a['sort'];
-					var d = b['sort'];
-				} else {
-					var c = b['sort'];
-					var d = a['sort'];
-				}
-
-				if (options.sort == 'date') {
-					return new Date(c) - new Date(d);
-				} else {
-					c = c.toLowerCase();
-					d = d.toLowerCase();
-					return (c < d) ? -1 : (c > d) ? 1 : 0;
-				}
-			});
-		}*/
-
-		// Add rows to output
 		$.each(rowArray, function(e) {
+			html += '<li class="rssRow '+ row +'">' + rowArray[e]['html'] + '</li>';
 
-			html += '<li class="rssRow '+row+'">' + rowArray[e]['html'] + '</li>';
-
-			// Alternate row classes
 			if (row == 'odd') {
 				row = 'even';
 			} else {
@@ -200,28 +153,32 @@
 			}
 		});
 
-		html += '</ul>' +
-			'</div>'
+		html += '</ul>' + '</div>'
 
 		$(e).html(html);
 
-		// Apply target to links
-		$('a',e).attr('target',options.linktarget);
-	};
+		$("a", e).attr("target", options.linktarget);
+}
 
-	var _formatFilesize = function(bytes) {
-		var s = ['bytes', 'kb', 'MB', 'GB', 'TB', 'PB'];
+function _formatFilesize(bytes) {
+		var s = [
+			'bytes',
+			'kb',
+			'MB',
+			'GB',
+			'TB',
+			'PB'
+		];
 		var e = Math.floor(Math.log(bytes)/Math.log(1024));
+
 		return (bytes/Math.pow(1024, Math.floor(e))).toFixed(2)+" "+s[e];
-	}
+}
 
-	var _formatDate = function(date,mask) {
-
-		// Convert to date and set return to the mask
+function _formatDate(date, mask) {
 		var fmtDate = new Date(date);
+
 		date = mask;
 
-		// Replace mask tokens
 		date = date.replace('dd', _formatDigit(fmtDate.getDate()));
 		date = date.replace('MMMM', _getMonthName(fmtDate.getMonth()));
 		date = date.replace('MM', _formatDigit(fmtDate.getMonth()+1));
@@ -231,14 +188,18 @@
 		date = date.replace('ss', _formatDigit(fmtDate.getSeconds()));
 
 		return date;
-	}
+}
 
-	var _formatDigit = function(digit) {
-		digit += '';
-		if (digit.length < 2) digit = '0' + digit;
+function _formatDigit(digit) {
+		digit += "";
+
+		if (digit.length < 2) {
+			digit = "0" + digit;
+		}
+
 		return digit;
-	}
-
+}
+// Todo
 	var _getMonthName = function(month) {
 		var name = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 		return name[month];
